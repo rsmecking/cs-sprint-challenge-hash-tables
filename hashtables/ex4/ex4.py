b45ae0359e676327cafd17c8a965af0c6fc425d0
Ryan Mecking <rsmecking@gmail.com>
2020-08-07 11:53:05 -0400

    Finsihed ex4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,9 +3,16 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+    v = []
 
-    return result
+    for num in a:
+        cache[num] = 1
+        if num != 0 and -num in cache:
+            v.append(abs(num))
+    return v
 
 
 if __name__ == "__main__":
     print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
+#https://www.geeksforgeeks.org/pairs-of-positive-negative-values-in-an-array/
